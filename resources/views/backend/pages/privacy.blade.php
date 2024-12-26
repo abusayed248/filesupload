3ee528144b2088b1e3f42b573749338df5cd32fd
abusayed248 <abusayed20012022@gmail.com>
2024-12-27 01:24:53 +0600

    design

---
+++ resources/views/backend/pages/privacy.blade.php
@@ -0,0 +1,173 @@
+
+@extends('layouts.app')
+
+@section('title', 'PRIVACY POLICY')
+
+@section('content')
+    <section class="">
+       <div class="container">
+           <div class="row>
+                    <div class="col-md-7  border-right">
+                        <div class="mt-5 p-4">
+                            <h5 class="border-bottom pb-3 fw-light">PRIVACY POLICY</h5>
+                            
+                            <div class="">
+                                <p class="fw-lighter">Privacy Policy for Ingenium Yazilim</p>
+
+                                <p class="fw-lighter mt-5">At Easyupload, accessible from https://easyupload.io, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by Easyupload and how we use it.</p>
+
+                                <p class="fw-lighter mt-5">If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
+                                <p class="fw-lighter mt-5">This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in Easyupload. This policy is not applicable to any information collected offline or via channels other than this website. Our Privacy Policy was created with the help of the Privacy Policy Generator.</p>
+                            </div>
+                            
+                            <div class="mt-3 py-5">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3 mt-3">Consent</h5>
+                                <p class="fw-lighter">By using our website, you hereby consent to our Privacy Policy and agree to its terms. </p>
+                            </div>
+                            
+                            
+                            
+                            <div class="mt-3 py-3">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3 mt-3">Information we collect </h5>
+
+                                <p class="fw-lighter">The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information. </p>
+
+                                <p class="fw-lighter">If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.</p>
+                                <p class="fw-lighter">When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.</p>
+                            </div>
+
+                            <div class="py-3">
+                                <h5 class="gray-200 fw-normal mt-3">How we use your information </h5>
+                                
+                                <p class="fw-lighter">We use the information we collect in various ways, including to:</p>
+
+                                <ul class="menu fw-lighter fw-italic">
+                                    <li>Provide, operate, and maintain our website</li>
+                                    <li>Improve, personalize, and expand our website</li>
+                                    <li>Understand and analyze how you use our website
+                                    </li>
+                                    <li>Develop new products, services, features, and functionality
+                                    </li>
+                                    <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes
+                                    </li>
+                                    <li>Send you emails
+                                    </li>
+                                    <li>Find and prevent fraud</li>
+                                </ul>
+                            </div>
+
+                            <div class="mt-3">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">Log Files</h5>
+                                <p class="fw-lighter">Easyupload follows a standard procedure of using log files. These files log visitors when they visit websites. All hosting companies do this and a part of hosting services' analytics. The information collected by log files include internet protocol (IP) addresses, browser type, Internet Service Provider (ISP), date and time stamp, referring/exit pages, and possibly the number of clicks. These are not linked to any information that is personally identifiable. The purpose of the information is for analyzing trends, administering the site, tracking users' movement on the website, and gathering demographic information.
+                                </p>
+                            </div>
+
+                            <div class="mt-3 py-5">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">Cookies and Web Beacons</h5>
+                                <p class="fw-lighter">Like any other website, Easyupload uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
+                                </p>
+
+                                <p class="fw-lighter">For more general information on cookies, please read "What Are Cookies".</p>
+                            </div>
+
+                            <div class="mt-3 py-3 take_down_notice">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">Google DoubleClick DART Cookie </h5>
+                                <p class="fw-lighter">Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – https://policies.google.com/technologies/ads
+
+                                </p>
+                            </div>
+
+                            <div class="mt-3 py-5">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">Our Advertising Partners </h5>
+                                <p class="fw-lighter">Some of advertisers on our site may use cookies and web beacons. Our advertising partners are listed below. Each of our advertising partners has their own Privacy Policy for their policies on user data. For easier access, we hyperlinked to their Privacy Policies below.</p>
+
+                                <p class="fw-lighter">https://policies.google.com/technologies/ads
+
+                                    <br>Google</p>
+                            </div>
+
+                            <div class="mt-3 py-3">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">Advertising Partners Privacy Policies </h5>
+                                <p class="fw-lighter">You may consult this list to find the Privacy Policy for each of the advertising partners of Easyupload.</p>
+                                <p class="fw-lighter">Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on Easyupload, which are sent directly to users' browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
+
+                                <p>Note that Easyupload has no access to or control over these cookies that are used by third-party advertisers.
+                                </p>
+                            </div>
+
+                            <div class="mt-3 py-3">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">Third Party Privacy Policies
+                                </h5>
+                                <p class="fw-lighter">Easyupload's Privacy Policy does not apply to other advertisers or websites. Thus, we are advising you to consult the respective Privacy Policies of these third-party ad servers for more detailed information. It may include their practices and instructions about how to opt-out of certain options.
+
+                                </p>
+                                <p class="fw-lighter">You can choose to disable cookies through your individual browser options. To know more detailed information about cookie management with specific web browsers, it can be found at the browsers' respective websites.</p>
+
+                            </div>
+
+                            <div class="mt-3 py-3">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">CCPA Privacy Rights (Do Not Sell My Personal Information)
+                                </h5>
+                                <p class="fw-lighter">Under the CCPA, among other rights, California consumers have the right to:</p>
+                                <p class="fw-lighter">Request that a business that collects a consumer's personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</p>
+                                <p class="fw-lighter">Request that a business delete any personal data about the consumer that a business has collected.</p>
+                                <p class="fw-lighter">Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</p>
+                                <p class="fw-lighter">Request that a business that sells a consumer's personal data, not sell the consumer's personal data.</p>
+                                <p class="fw-lighter">If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.</p>
+
+                            </div>
+                            </div>
+
+                            <div class="mt-3 py-3">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">GDPR Data Protection Rights </h5>
+                                <p class="fw-lighter">We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:
+                                </p>
+                                <p class="fw-lighter">The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.
+
+                                </p>
+                                <p class="fw-lighter">The right to erasure – You have the right to request that we erase your personal data, under certain conditions.
+
+                                </p>
+                                <p class="fw-lighter">The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.
+
+                                </p>
+                                <p class="fw-lighter">The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.
+
+                                </p>
+                                <p class="fw-lighter">The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.
+                                </p>
+                                <p class="fw-lighter">If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+
+                                </p>
+
+                            </div>
+
+                            <div class="mt-3 py-3">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">Children's Information</h5>
+                                <p class="fw-lighter">Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity. </p>
+                                <p class="fw-lighter">Easyupload does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.  </p>
+                            </div>
+
+                            <div class="mt-3 py-3">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">Common ID Cookie</h5>
+                                <p class="fw-lighter">This site uses cookies and similar tracking technologies such as the Common ID cookie to provide its services. Cookies are important devices for measuring advertising effectiveness and ensuring a robust online advertising industry. The Common ID cookie stores a unique user id in the first party domain and is accessible to our ad partners. This simple ID that can be utilized to improve user matching, especially for delivering ads to iOS and MacOS browsers. Users can opt out of the Common ID tracking cookie by clicking here.</p>
+                            </div>
+
+                            <div class="mt-3 pb-2">
+                                <h5 class="pb-3 gray-200 fw-normal mb-3">Advertising Privacy Settings</h5>
+                                <p class="fw-lighter">FOR EU USERS ONLY: When you use our site, pre-selected companies may access and use certain information on your device and about your interests to serve ads or personalized content. You may revisit or change consent-choices at any time by clicking here.</p>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div> 
+     
+    </section>
+
+   
+@endsection
+   
+ 
+    
+
