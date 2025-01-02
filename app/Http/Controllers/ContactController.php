3b8d9f29fccaa0895f896e6d03aa084ef9a000a1
abusayed248 <abusayed20012022@gmail.com>
2025-01-02 09:54:14 +0600

    dn_contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Mail\ContactMail;
+use App\Models\ContactInfo;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
    public function index()
    {
-       return view('backend.pages.contact');
+        $contact_info = ContactInfo::first();
+       return view('backend.pages.contact', compact('contact_info'));
    }
 
    // terms page
@@ -36,5 +40,50 @@ class ContactController extends Controller
 
     Mail::to($request->email)->send(new ContactMail($mailData));
     return redirect()->back()->with('status', 'Send your mail successfully.');
+   }
+   // update contact info
+   public function updateContactInfo()
+   {
+    $info = ContactInfo::first();
+    return view('backend.pages.edit-contact-info', compact('info'));
+   }
+   // update contact info status
+   public function updateContactInfoStatus(Request $request)
+   {
+        $contact_info = ContactInfo::find($request->id);
+
+        if ($contact_info) {
+            // Update the fields
+            $contact_info->update([
+                'company_name' => $request->company_name,
+                'text_no' => $request->text_no,
+                'address' => $request->address,
+                'tel' => $request->tel,
+                'email' => $request->email,
+                'twitter' => $request->twitter,
+                'description' => $request->description,
+            ]);
+        
+            // Handle the photo upload
+            $photo = $request->file('photo');
+            $slug = Str::slug($request->company_name, '-');
+        
+            if ($photo) {
+
+                if ($request->old_photo && File::exists(public_path($request->old_photo))) {
+                    File::delete(public_path($request->old_photo));
+                }
+                
+                $extension = $photo->getClientOriginalExtension();
+                $fileNameToStore = $slug . '_' . time() . '.' . $extension; // Filename to store
+                $destinationPath = 'files/contact_info';
+                $photo->move(public_path($destinationPath), $fileNameToStore);
+        
+                // Update the photo field
+                $contact_info->photo = $destinationPath . '/' . $fileNameToStore;
+                $contact_info->save();
+            }
+        }
+        return redirect()->back()->with('infostatus', 'Contact infos updated successfully.');
    }
 }
---
+++ database/migrations/2025_01_01_183642_create_contact_infos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name')->nullable();
+            $table->string('text_no')->nullable();
+            $table->string('address')->nullable();
+            $table->string('tel')->nullable();
+            $table->string('email')->nullable();
+            $table->string('twitter')->nullable();
+            $table->string('photo')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_infos');
+    }
+};
